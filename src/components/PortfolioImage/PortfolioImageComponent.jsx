b3e771fc4730ad3a8ecaf4461a2975be43c3efce
Carey Best <carey@stubbystudios.com>
2020-07-14 17:25:42 -0500

    Turning off modals for mobile

--- src/components/PortfolioImage/PortfolioImageComponent.jsx
+++ src/components/PortfolioImage/PortfolioImageComponent.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './PortfolioImage.scss';
+import { useMediaQuery } from 'react-responsive';
 
 const PortfolioImage = (props) => {
+  const isMediumScreen = useMediaQuery({
+    query: '(min-device-width: 768px)'
+  })
   const portfolio = props.item.portfolio;
   return portfolio.map((screenshot, index) =>
     <li className='screenshot' key={index}>
       <div className='thumbnail-content'>
-        <img
-          src={screenshot.thumbnail}
-          alt={screenshot.thumbnail_alt}
-          onClick={() => props.handleClick(screenshot)}
-        />
+        {isMediumScreen
+          ? <img
+            src={screenshot.thumbnail}
+            alt={screenshot.thumbnail_alt}
+            onClick={() => props.handleClick(screenshot)}
+          />
+          : <img
+            src={screenshot.thumbnail}
+            alt={screenshot.thumbnail_alt} />}
         <h3>{screenshot.title}</h3>
         <p>{screenshot.text}</p>
       </div>
